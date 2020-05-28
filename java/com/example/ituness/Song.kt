7180ca17faf9d09aaa9d94f3202786365b5e9f59
Himanshu Garg <hgarg701@gmail.com>
2020-05-28 15:50:47 -0700

    added dao for database queries

--- java/com/example/ituness/Song.kt
+++ java/com/example/ituness/Song.kt
@@ -1,11 +1,21 @@
 package com.example.ituness
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "songs")
 data class Song(
+
+    @PrimaryKey
     val songName: String,
 
+    @ColumnInfo(name = "singer")
     val singerName : String,
 
+    @ColumnInfo(name = "album")
     val albumName : String,
 
+    @ColumnInfo(name = "album_image")
     val imageUrl : String) {
 }
---
+++ java/com/example/ituness/SongDao.kt
@@ -0,0 +1,23 @@
+package com.example.ituness
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface SongDao {
+
+    @Insert
+    fun insert(song : Song)
+
+    @Delete
+    fun delete(song : Song)
+
+    @Query("SELECT * FROM songs")
+    fun  getAllSongs()
+
+    @Query("DELETE FROM songs")
+    fun deleteAllSongs()
+    
+}
